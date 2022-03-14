705666e7686caea4955018b7f7d8c8e79cfcf16a
GitHub Action <action@github.com>
2022-03-14 07:37:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/zercatmr/idea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zercatmr/idea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zercatmr.idea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zercatmr.idea.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/zercatmr/idea/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zercatmr/idea/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zercatmr.idea.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zercatmr.idea.MyBundle
 
 class MyProjectService(project: Project) {
 
